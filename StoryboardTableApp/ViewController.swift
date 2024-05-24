b9422e9c684683d5a3a2e2629d6d7c94b435d86f
wglee0511 <ranazang@naver.com>
2024-05-24 16:22:14 +0900

    feature: select event

--- StoryboardTableApp/ViewController.swift
+++ StoryboardTableApp/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let tableArray = ["배", "사과", "포도", "체리"]
+    let language = ["C++", "Swift", "Javascript", "Java", "C#", "Rust"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return tableArray.count
+        case 1:
+            return language.count
+        default:
+            return 0
+        }
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = tableArray[indexPath.row]
+        case 1:
+            cell.textLabel?.text = language[indexPath.row]
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+}
+
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+    }
+}
